3720c365d1c0a4ec2b91f9b6f6762354320f8967
vananh0909 <anhb2014814@student.ctu.edu.vn>
2023-09-22 16:41:25 +0700

    modified hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
     public static void main(String []args){
-        System.out.println("hello");
+        System.out.println("hello \n");
+        System.out.println("B2014814");
     }
 
 }
